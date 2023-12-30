da9bc14b8c30aab64abece721aa53ed4deae940e
En Wichtel <enwichtel@diegrossewichtelei.com>
2023-12-30 13:41:06 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "A project with a devshell.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    devshell.url = "github:numtide/devshell";
+    devshell.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devshell.flakeModule
+        # To import a flake module
+        # 1. Add foo to inputs
+        # 2. Add foo as a parameter to the outputs function
+        # 3. Add here: foo.flakeModule
+
+      ];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        # Per-system attributes can be defined here. The self' and inputs'
+        # module parameters provide easy access to attributes of the same
+        # system.
+
+        # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
+        packages.default = pkgs.hello;
+        devshells.default = {
+          env = [
+              # { name = "MY_ENV_VAR"; value = "SOTRUE"; }
+          ];
+          packages = [
+            pkgs.racket
+          ];
+          commands = [
+            { name = "r"; command = "racket"; help = "Run racket interactively";}
+          ];
+        };
+      };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+
+      };
+    };
+}
